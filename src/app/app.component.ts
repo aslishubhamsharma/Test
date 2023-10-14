b6f02e8253b5619fe768b96aa9e883bba2a36d8d
sagar <rahul@softassociates.com>
2023-10-14 13:32:13 +0530

    install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { NgModule, Component, enableProdMode } from '@angular/core';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { Company, Service } from './app.service';
+
+if (!/localhost/.test(document.location.host)) {
+  enableProdMode();
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app',
+  providers: [Service],
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title = 'test';
+  companies: any;
+
+  labelMode: any;
+
+  labelLocation: any;
+
+  readOnly: any;
+
+  showColon: any;
+
+  minColWidth: any;
+
+  colCount: any;
+
+  width: any;
+
+  constructor(service: Service) {
+    this.labelMode = 'floating';
+    this.labelLocation = 'left';
+    this.readOnly = false;
+    this.showColon = true;
+    this.minColWidth = 300;
+    this.colCount = 2;
+    this.companies = service.getCompanies();
+  }
+
+  getCompanySelectorLabelMode() {
+    return this.labelMode === 'outside'
+      ? 'hidden'
+      : this.labelMode;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  DxCheckBoxModule,
+  DxSelectBoxModule,
+  DxNumberBoxModule,
+  DxFormModule,
+} from 'devextreme-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+    declarations: [
+      AppComponent
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      DxCheckBoxModule,
+      DxSelectBoxModule,
+      DxNumberBoxModule,
+      DxFormModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
 export class AppModule { }
